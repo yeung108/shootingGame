4b1f2af435414cd29cb042b8e5a3a523bdda1ff8
Luster Wong <yeshin2008@gmail.com>
2018-04-29 00:10:47 +0800

    New Feature: More usage for the money in the game

---
+++ Assets/Script/GoldScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Fungus;
+
+public class GoldScript : MonoBehaviour {
+
+	public GameObject goldTextObject;
+	public int hintsPrice = 50;
+	public Flowchart flowChart;
+
+	// Update is called once per frame
+	void Update () {
+		goldTextObject.GetComponent<TextMeshProUGUI> ().text = "$" + SaveManager.Instance.state.gold;
+		flowChart.SetIntegerVariable ("gold", SaveManager.Instance.state.gold);
+	}
+
+	public void buyHints(){
+		SaveManager.Instance.state.gold -= hintsPrice;
+		SaveManager.Instance.Save ();
+	}
+
+}
